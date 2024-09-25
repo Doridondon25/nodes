b4ad5f2d6df26e1f09952a457f34f8687a1b5590
Doridondon25 <dori.donitza@gmail.com>
2024-09-25 22:36:33 +0300

    Users Commends in PostMan

--- node.js/index.js
+++ node.js/index.js
@@ -1,68 +1,65 @@
-// console.log("hello, Node.js!");
-// const fs = require('fs');
-// const path = require('path');
-// const http = require('http');
-// const { users, getUser } = require('./users');
-
-// // http.createServer((req, res) => {
-// //     res.writeHead(200, {'Content-Type': 'text/plain'});
-// //     res.end('Hello, world');
-// // }).listen(3000, () => {
-// //     console.log('Server running on port 3000');
-// // });
-
-// console.log(path.join(__dirname, 'hello.txt'));
-
-// function createFile() {
-//     fs.writeFileSync('hello.txt', "my name is dori");
-// }
-
-// createFile();
-
-// function readFile() {
-//     const data = fs.readFileSync('hello.txt', 'utf8');
-//     console.log(data);
-// }
-
-// readFile();
-
-// console.log(users[0].name);                                                                                                                                                                                                                                                                                     mn       
-// console.log(getUser(1).name);
-
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 app.use(express.json());
 
-
 const users = [
-  { id: 1, name: 'dori donitza'},
-  { id: 2, name: 'itay lol'},
-  { id: 3, name: 'eitan levi'},
+  { id: 1, name: 'dori donitza' },
+  { id: 2, name: 'itay lol' },
+  { id: 3, name: 'eitan levi' },
 ];
 
+// Get all users
 app.get('/users', (req, res) => {
   return res.json(users);
 });
 
+// Add a new user
 app.post('/add-user', (req, res) => {
-  const {name} = req.body;
+  const { name } = req.body;
   if (!name) {
     return res.status(400).json({ error: 'name is required' });
   }
-const existingUser = users.find(user => user.name === name);
+  const existingUser = users.find(user => user.name === name);
   if (existingUser) {
     return res.status(400).json({ error: 'name already exists' });
   }
-  //if (users.find(user => user.name === name)) {
-    //return res.status(400).json({ error: 'name already exists' });
-  //}
 
   const newuser = { id: users.length + 1, name };
   users.push(newuser);
   return res.status(201).json(users);
 });
 
-app.listen(3000, () => {
-    console.log('server is runnuing on http://localhost:3000');
+// Change user name
+app.patch('/change-name', (req, res) => {
+  const { id } = req.body;
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: 'name is required' });
+  }
+
+  const user = users.find(user => user.id === parseInt(id));
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+  user.name = name;
+  return res.json(users);
 });
-//add test data to commit
+
+// Delete a user by ID
+app.delete('/delete-user', (req, res) => {
+  const { id } = req.body;
+  const userIndex = users.findIndex(user => user.id === parseInt(id));
+
+  if (userIndex === -1) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  users.splice(userIndex, 1);
+  return res.json({ message: `User with ID ${id} deleted`, users });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
